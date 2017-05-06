8cad3a9583b0c7173aba2600d02284ecf860e7b4
Ankur Singh <ankur13019@iiitd.ac.in>
2017-05-06 22:48:58 +0530

    added slider and sidebar

--- curations/js/main.js
+++ curations/js/main.js
@@ -1,4 +1,23 @@
-var app = new Vue({
-  el: '#app',
-  data: data["1"]
-})
+$(document).ready(function(){
+
+    var app = new Vue({
+        el: '#app',
+        data: data["1"]
+    });
+    var owl = $(".owl-carousel").owlCarousel({
+        items:1,
+        loop:false,
+        center:true,
+        margin:10,
+        URLhashListener:true,
+        startPosition: 'URLHash',
+        nav: true,
+        dots: true,
+        autoHeight:true,
+        navContainer: ".carousel-nav",
+        dotsContainer: ".carousel-dots"
+    });
+    embedly('on', 'card.rendered', function(iframe){
+        owl.trigger('refresh.owl.carousel');
+    });
+});
