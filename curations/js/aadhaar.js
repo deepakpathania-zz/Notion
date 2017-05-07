cacaf4157d4bcb34df5e7c91f2bff071cca985c4
Samarth Bansal <samarth@Samarths-MacBook-Pro.local>
2017-05-07 14:18:29 +0530

    f

---
+++ curations/js/aadhaar.js
@@ -0,0 +1,156 @@
+var data1 = {
+  "id": 1,
+  "topic": "Aadhaar",
+  "curation_type": "community",
+  "author": "Ankit Mehta",
+  "created_at": "Mays 5, 2017 2:00",
+  "last_updated": "May 6, 2017 17:00",
+  "curation_desc": "In these pages, I explain the Aadhaar controversy.",
+  "content":[
+      {
+        "heading": "Advantages of Aadhar",
+        "heading_id": 1,
+        "head_desc": "Why is the government so enthusiastic about this project?",
+        "elements":[
+            {
+              "element_id": 1,
+              "url": "https://thewire.in/24713/aadhaar-identification-simplified-myths-busted/",
+              "type": "article",
+              "headline": "You need Aadhaar for all these services",
+              "comment": "Some myth busting here!",
+              "selected_text": "Of the 61 services for which Aadhaar is required, all, barring 10, are welfare schemes."
+            },
+            {
+              "element_id": 2,
+              "url": "https://uidai.gov.in/images/aadhaar_question_and_answers.pdf",
+              "type": "article",
+              "headline": "You need Aadhaar for all these services",
+              "comment": "Government explains",
+              "selected_text": "Of the 61 services for which Aadhaar is required, all, barring 10, are welfare schemes."
+            },
+            {
+              "element_id": 3,
+              "url": "http://www.business-standard.com/article/opinion/the-benefits-of-aadhaar-115010701230_1.html",
+              "type": "article",
+              "headline": "You need Aadhaar for all these services",
+              "comment": "Business Standard lists down the benefits of Aadhaar",
+              "selected_text": "Of the 61 services for which Aadhaar is required, all, barring 10, are welfare schemes."
+            }
+
+          ]
+      },
+      {
+        "heading": "Privacy and data security issues",
+        "heading_id": 2,
+        "head_desc": "Is Aadhaar too good a system for a country like India?",
+        "elements":[
+            {
+              "element_id": 1,
+              "url": "http://www.hindustantimes.com/india/privacy-concerns-overshadow-monetary-benefits-of-aadhaar-scheme/story-E3o0HRwc6XOdlgjqgmmyAM.html",
+              "type": "article",
+              "headline": "You need Aadhaar for all these services",
+              "comment": "Privacy first!",
+              "selected_text": "Of the 61 services for which Aadhaar is required, all, barring 10, are welfare schemes."
+            },
+            {
+              "element_id": 2,
+              "url": "http://www.livemint.com/Opinion/VSqpBps7Y5YrUhvS5mGgSO/Aadhaar-still-too-many-problems.html",
+              "type": "article",
+              "headline": "You need Aadhaar for all these services",
+              "comment": "Will the government answer?"
+            },
+            {
+              "element_id": 3,
+              "url": "http://www.business-standard.com/article/economy-policy/aadhaar-breach-exposes-ecosystem-s-vulnerabilities-117030300961_1.html",
+              "type": "article",
+              "headline": "You need Aadhaar for all these services",
+              "comment": "Vulnerabilities in the Aadhaar ecosystem."
+            },
+
+            {
+              "element_id": 3,
+              "url": "http://www.livemint.com/Opinion/3kjHJ0Z7cwhuBgxETejCAI/Aadhaar-and-the-right-to-privacy.html",
+              "type": "article",
+              "headline": "You need Aadhaar for all these services",
+              "comment": "In India, there is no right to privacy—yet!"
+            }
+          ],     
+      },
+      {
+        "heading": "Aadhaar bill controversy",
+        "heading_id": 3,
+        "head_desc": "Why passed as a money bill?",
+        "elements":[
+            {
+              "element_id": 1,
+              "url": "http://www.prsindia.org/uploads/media/UID/Legislative%20Brief%20-%20UID%20Bill%202010_June%202.pdf",
+              "type": "article",
+              "headline": "You need Aadhaar for all these services",
+              "comment": "For deeper legal understanding, read.",
+              "selected_text": ""
+            },
+            {
+              "element_id": 2,
+              "url": "http://www.prsindia.org/billtrack/the-aadhaar-targeted-delivery-of-financial-and-other-subsidies-benefits-and-services-bill-2016-4202/",
+              "type": "article",
+              "headline": "You need Aadhaar for all these services",
+              "comment": "There are more!"
+            },
+            {
+              "element_id": 3,
+              "url": "http://www.prsindia.org/uploads/media/AADHAAR/Bill%20Summary-%20Aadhaar%20Bill.pdf",
+              "type": "article",
+              "headline": "You need Aadhaar for all these services",
+              "comment": "Aadhaar bill, summarised!"
+            },
+
+            {
+              "element_id": 3,
+              "url": "http://www.livemint.com/Opinion/3kjHJ0Z7cwhuBgxETejCAI/Aadhaar-and-the-right-to-privacy.html",
+              "type": "article",
+              "headline": "You need Aadhaar for all these services",
+              "comment": "In India, there is no right to privacy—yet!"
+            },
+
+            {
+              "element_id": 3,
+              "url": "http://www.livemint.com/Opinion/aDeloJRt0lYN9wyt0ETn5N/Building-a-legal-ecosystem-for-Aadhaar.html",
+              "type": "article",
+              "headline": "You need Aadhaar for all these services",
+              "comment": "Arguably the best piece on Aadhaar"
+            }
+          ]     
+      },
+
+      {
+        "heading": "Detailed reports",
+        "heading_id": 4,
+        "head_desc": "If you want to dive deep, you may like to read these!",
+        "elements":[
+            {
+              "element_id": 1,
+              "url": "http://www.prsindia.org/uploads/media/UID/uid%20report.pdf",
+              "type": "article",
+              "headline": "You need Aadhaar for all these services",
+              "comment": "Standing committee report of Parliament",
+              "selected_text": ""
+            },
+            {
+              "element_id": 2,
+              "url": "http://www.cse.iitd.ernet.in/~suban/reports/aadhaar.pdf",
+              "type": "article",
+              "headline": "You need Aadhaar for all these services",
+              "comment": "Researchers at IIT Delhi investigate Aadhaar concerns from a technical perspective"
+            },
+            {
+              "element_id": 3,
+              "url": "https://scroll.in/topic/38792/identity-project",
+              "type": "article",
+              "headline": "You need Aadhaar for all these services",
+              "comment": "Scroll's ground reportage across the country on Aadhaar"
+            },
+          ]     
+      },
+
+    ]
+}
